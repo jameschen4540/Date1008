a43a11cbd2d9b77471ab07082d6dca822d8fe7bb
jameschen4540 <541930242@qq.com>
2016-10-13 15:52:20 +0800

    this is a waveview

---
+++ app/src/main/java/com/ch/date1008/CountDownView.java
@@ -0,0 +1,198 @@
+package com.ch.date1008;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.os.CountDownTimer;
+import android.text.Layout;
+import android.text.StaticLayout;
+import android.text.TextPaint;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * 项目名称：Date1008
+ * 类描述：
+ * 创建人：陈志平
+ * 创建时间：2016/10/13  11:35
+ */
+public class CountDownView extends View {
+
+    private static final String TAG = CountDownView.class.getSimpleName();
+    private static final int BACKGROUND_COLOR = 0x50555555;
+    private static final float BORDER_WIDTH = 15f;
+    private static final int BORDER_COLOR = 0xFF6ADBFE;
+    private static final String TEXT = "跳过广告";
+    private static final float TEXT_SIZE = 50f;
+    private static final int TEXT_COLOR = 0xFFFFFFFF;
+
+    private int backgroundColor;
+    private float borderWidth;
+    private int borderColor;
+    private String text;
+    private int textColor;
+    private float textSize;
+
+    private Paint cyclerPaint;
+    private Paint borderPaint;
+    private TextPaint textPaint;
+    private Canvas cyclerCanvas;
+    private StaticLayout staticLayout;
+    private float progress;
+    private CountDownViewListener listener;
+
+
+    public CountDownView(Context context) {
+        this(context, null);
+    }
+
+    public CountDownView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public CountDownView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+
+        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.CountDownView);
+        backgroundColor = typedArray.getColor(R.styleable.CountDownView_background_color, BACKGROUND_COLOR);
+        borderColor = typedArray.getColor(R.styleable.CountDownView_border_color, BORDER_COLOR);
+        borderWidth = typedArray.getDimension(R.styleable.CountDownView_border_width, BORDER_WIDTH);
+        text = typedArray.getString(R.styleable.CountDownView_text);
+        textColor = typedArray.getColor(R.styleable.CountDownView_textColor, TEXT_COLOR);
+        textSize = typedArray.getDimension(R.styleable.CountDownView_textSize, TEXT_SIZE);
+        if (text == null) {
+            text = TEXT;
+        }
+
+        typedArray.recycle();
+
+        init();
+
+
+    }
+
+    private void init() {
+
+        cyclerCanvas = new Canvas();
+        cyclerPaint = new Paint();
+        cyclerPaint.setAntiAlias(true);
+        cyclerPaint.setColor(backgroundColor);
+        cyclerPaint.setStyle(Paint.Style.FILL);
+        cyclerPaint.setDither(true);
+        cyclerPaint.setAlpha(60);
+
+
+        textPaint = new TextPaint();
+        textPaint.setTextSize(textSize);
+        textPaint.setColor(textColor);
+        textPaint.setAntiAlias(true);
+        textPaint.setTextAlign(Paint.Align.CENTER);
+
+        borderPaint = new Paint();
+        borderPaint.setAntiAlias(true);
+        borderPaint.setColor(borderColor);
+        borderPaint.setStyle(Paint.Style.STROKE);
+        borderPaint.setStrokeWidth(borderWidth);
+        borderPaint.setDither(true);
+        borderPaint.setAlpha(90);
+
+        int textWidth = (int) textPaint.measureText(text, 0, (text.length() + 1) / 2);
+
+
+        staticLayout = new StaticLayout(text, textPaint, textWidth, Layout.Alignment.ALIGN_NORMAL, 1f, 0, false);
+
+
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+        int widthSize = MeasureSpec.getSize(widthMeasureSpec);
+        int widthMode = MeasureSpec.getMode(widthMeasureSpec);
+        int heightSize = MeasureSpec.getSize(heightMeasureSpec);
+        int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+
+//        int widthPixels = getResources().getDisplayMetrics().widthPixels;
+//        int heightPixels = getResources().getDisplayMetrics().heightPixels;
+
+
+//        if (widthMode != MeasureSpec.EXACTLY) {
+//            widthSize = staticLayout.getWidth()*2;
+//        }
+        if (widthMode == MeasureSpec.AT_MOST) {
+            widthSize = (int) ((staticLayout.getWidth()+borderWidth) * 2);
+        }
+
+//        if (heightMode != MeasureSpec.EXACTLY) {
+//            heightSize = staticLayout.getHeight()*2;
+//        }
+
+        if (heightMode == MeasureSpec.AT_MOST) {
+            heightSize = (int) ((staticLayout.getHeight()+borderWidth) * 2);
+        }
+
+        setMeasuredDimension(widthSize, heightSize);
+
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        int height = getMeasuredHeight();
+        int width = getMeasuredWidth();
+        int min = Math.min(width, height);
+
+        //画圆
+        canvas.drawCircle(width / 2, height / 2, min / 2, cyclerPaint);
+
+        RectF rectF;
+        if (width > height) {
+            rectF = new RectF((width - min) / 2 + borderWidth / 2, borderWidth / 2, (width + min) / 2 - borderWidth / 2, height - borderWidth / 2);
+        } else {
+            rectF = new RectF(borderWidth / 2, height / 2 - min / 2 + borderWidth / 2, width - borderWidth / 2, height / 2 + min / 2 - borderWidth / 2);
+        }
+
+        //画边缘线
+        canvas.drawArc(rectF, -90, progress, false, borderPaint);
+        // canvas.drawText("稍等片刻", width / 2, height / 2 - textPaint.descent() + textPaint.getTextSize() / 2, textPaint);
+        canvas.translate(width / 2, height / 2 - staticLayout.getHeight() / 2);
+
+        staticLayout.draw(canvas);
+
+        invalidate();
+    }
+
+    public void start(final int time) {
+
+        CountDownTimer countDownTimer = new CountDownTimer(time, 60) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                progress = ((time - millisUntilFinished) / (float) time) * 360;
+            }
+
+            @Override
+            public void onFinish() {
+                progress = 360;
+                listener.onFinshCount();
+                invalidate();
+            }
+        }.start();
+    }
+
+    public void setCountDownViewListener(CountDownViewListener listener) {
+        this.listener = listener;
+    }
+
+    interface CountDownViewListener {
+
+        void onStartCount();
+
+        void onFinshCount();
+
+
+    }
+
+}
--- app/src/main/java/com/ch/date1008/SplashActivity.java
+++ app/src/main/java/com/ch/date1008/SplashActivity.java
@@ -2,11 +2,9 @@ package com.ch.date1008;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.CountDownTimer;
 import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.widget.TextView;
 
 /**
@@ -15,17 +13,21 @@ import android.widget.TextView;
  * 创建人：陈志平
  * 创建时间：2016/10/9  15:45
  */
-public class SplashActivity extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity implements CountDownView.CountDownViewListener {
 
     private TextView btn_time;
-    private Handler handler=new Handler();
+    private Handler handler = new Handler();
     private Runnable runnable;
+    private CountDownView countDownView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_spash);
         btn_time = ((TextView) findViewById(R.id.btn_time));
+        countDownView = ((CountDownView) findViewById(R.id.count_down_view));
+        countDownView.start(10000);
+        countDownView.setCountDownViewListener(this);
 
 
         // 判断是否是第一次开启应用
@@ -38,34 +40,34 @@ public class SplashActivity extends AppCompatActivity {
             return;
         }
 
-        final CountDownTimer downTimer = new CountDownTimer(3000, 1000) {
-            @Override
-            public void onTick(long millisUntilFinished) {
-                btn_time.setText("跳过广告");
-            }
+//        final CountDownTimer downTimer = new CountDownTimer(3000, 1000) {
+//            @Override
+//            public void onTick(long millisUntilFinished) {
+//                btn_time.setText("跳过广告");
+//            }
+//
+//            @Override
+//            public void onFinish() {
+//               // btn_time.setText("倒计时"+0+"s");
+//                Intent i=new Intent(SplashActivity.this,MainActivity.class);
+//                startActivity(i);
+//                finish();
+//            }
+//        };
+//        downTimer.start();
 
-            @Override
-            public void onFinish() {
-               // btn_time.setText("倒计时"+0+"s");
-                Intent i=new Intent(SplashActivity.this,MainActivity.class);
-                startActivity(i);
-                finish();
-            }
-        };
-        downTimer.start();
-
-        btn_time.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i=new Intent(SplashActivity.this,MainActivity.class);
-                startActivity(i);
-                finish();
-                downTimer.cancel();
-//                if (runnable != null) {
-//                    handler.removeCallbacks(runnable);
-//                }
-            }
-        });
+//        btn_time.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent i=new Intent(SplashActivity.this,MainActivity.class);
+//                startActivity(i);
+//                finish();
+//                downTimer.cancel();
+////                if (runnable != null) {
+////                    handler.removeCallbacks(runnable);
+////                }
+//            }
+//        });
 
 //
 //        handler.postDelayed(runnable=new Runnable() {
@@ -76,6 +78,19 @@ public class SplashActivity extends AppCompatActivity {
 //                finish();
 //            }
 //        },3000);
+
+    }
+
+    @Override
+    public void onStartCount() {
+
+    }
+
+    @Override
+    public void onFinshCount() {
+        Intent i = new Intent(SplashActivity.this, MainActivity.class);
+        startActivity(i);
+        finish();
 
     }
 }
